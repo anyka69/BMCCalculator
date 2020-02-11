60b52f819884a3e615b39f23899c912a110ae65a
Anna <anyka69@mail.ru>
2020-02-11 23:34:16 +0300

    Added:Category is Weight (kilogram or pounds)

--- src/test/java/BMICalculatorNormal.java
+++ src/test/java/BMICalculatorNormal.java
@@ -57,4 +57,22 @@ public class BMICalculatorNormal {
 
         browser.quit();
     }
+
+    @Test
+    public void categoryWeight() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+
+        WebDriver browser = new ChromeDriver();
+        browser.get("https://healthunify.com/bmicalculator/");
+        Select weight = new Select(browser.findElement(By.name("opt1")));
+        browser.findElement(By.name("wg")).sendKeys("44");
+        weight.selectByVisibleText("pounds");
+        browser.findElement(By.name("ht")).sendKeys("150");
+        browser.findElement(By.name("cc")).click();
+        String category = browser.findElement(By.name("desc")).getAttribute("value");
+        assertEquals(category, "Your category is Obese", "Категория неверная");
+
+        browser.quit();
+
+    }
 }
