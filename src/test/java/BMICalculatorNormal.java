1690122b8dd922a5e09e7efe39dd72eae6408b1f
Anna <anyka69@mail.ru>
2020-02-11 23:31:22 +0300

    Added:Category is Underweight

--- src/test/java/BMICalculatorNormal.java
+++ src/test/java/BMICalculatorNormal.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
@@ -23,7 +24,7 @@ public class BMICalculatorNormal {
     }
 
     @Test
-    public void categoryTest() {
+    public void categoryStarvation() {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
 
         WebDriver browser = new ChromeDriver();
@@ -34,6 +35,25 @@ public class BMICalculatorNormal {
         browser.findElement(By.name("cc")).click();
         String category = browser.findElement(By.name("desc")).getAttribute("value");
         assertEquals(category, "Your category is Starvation", "Категория неверная");
+
+        browser.quit();
+    }
+
+    @Test
+    public void categoryUnderweight() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+
+        WebDriver browser = new ChromeDriver();
+        browser.get("https://healthunify.com/bmicalculator/");
+
+        browser.findElement(By.name("wg")).sendKeys("55");
+        Select height = new Select(browser.findElement(By.name("opt2")));
+        height.selectByIndex(4);
+        Select height1 = new Select(browser.findElement(By.name("opt3")));
+        height.selectByIndex(5);
+        browser.findElement(By.name("cc")).click();
+        String category = browser.findElement(By.name("desc")).getAttribute("value");
+        assertEquals(category, "Your category is Underweight", "Категория неверная");
 
         browser.quit();
     }
