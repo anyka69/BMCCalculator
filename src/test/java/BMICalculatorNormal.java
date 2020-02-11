c44e73e0ccedcc0545d44cc0f1f8fae3016b413a
Anna <anyka69@mail.ru>
2020-02-11 23:28:54 +0300

    Added:Category is Starvation

--- src/test/java/BMICalculatorNormal.java
+++ src/test/java/BMICalculatorNormal.java
@@ -20,7 +20,21 @@ public class BMICalculatorNormal {
         assertEquals(category, "Your category is Normal", "Категория неверная");
 
         browser.quit();
+    }
 
+    @Test
+    public void categoryTest() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
 
+        WebDriver browser = new ChromeDriver();
+        browser.get("https://healthunify.com/bmicalculator/");
+
+        browser.findElement(By.name("wg")).sendKeys("20");
+        browser.findElement(By.name("ht")).sendKeys("150");
+        browser.findElement(By.name("cc")).click();
+        String category = browser.findElement(By.name("desc")).getAttribute("value");
+        assertEquals(category, "Your category is Starvation", "Категория неверная");
+
+        browser.quit();
     }
 }
